57d846b6ff600edc46e2401a0d246f666d06dec8
lucascheco <lmatheus.sp@gmail.com>
2021-01-31 09:19:37 -0300

    Revisao

---
+++ Estrutura_de_Dados/ponteiros.c
@@ -0,0 +1,22 @@
+
+/***********************************************************************************
+    Author:      Lucas Pacheco.
+    Description: Pointers
+    Date:        31/01/2021
+************************************************************************************/
+typedef int  variavel;
+typedef int* ponteiro;
+
+/*
+ * VARIÁVEL = valor guardado na memória no qual atribuímos um nome.
+ 
+ * PONTEIRO -> variável cujo valor é um endereço de memória.
+  
+ * Acho que os dois pontos acima são suficientes para mostrar a relação,
+   as semelhanças e as principais diferenças.
+
+ * Em C/C++ declaramos ponteiros da mesma forma em que declaramos as 
+   variáveis com adiçao de um '*' na frente do nome atribuído à vari-
+   ável.
+*/
+
