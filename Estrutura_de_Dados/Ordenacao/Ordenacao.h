09102ebcd59f7fb01c24b9b4ee9044714143afe5
lucascheco <lmatheus.sp@gmail.com>
2021-05-14 10:53:54 -0300

    Algoritmos de Ordenacao

--- Estrutura_de_Dados/Ordenacao/Ordenacao.h
+++ Estrutura_de_Dados/Ordenacao/Ordenacao.h
@@ -1,16 +1,41 @@
-#define MAXVET 1000
+/***********************************************************************************
+    Author:      Lucas Pacheco.
+    Description: Protótipos de funções de ordenação.
+    Date:        13/05/2021
+************************************************************************************/
+
+
+#define MAXVET 10000000
 
 typedef int vetor[MAXVET];
 
+typedef struct desempenho
+{
+    long int tempo;
+    double nCompara,nOpera;
+} Desempenho;
 
 void geraVetor(vetor V, int tam);
 
-void mostraVetor(vetor V, int tam);
-
-void selectionSort(vetor V, int tam);
-
-void insertionSort(vetor V, int tam);
-
-void bubbleSort   (vetor V, int tam);
+void mostraVetor(vetor V, int tam, char *mensagem);
 
 
+Desempenho selectionSort(vetor V, int tam);
+
+Desempenho insertionSort(vetor V, int tam);
+
+Desempenho bubbleSort   (vetor V, int tam);
+
+Desempenho shellSort    (vetor V, int tam);
+
+Desempenho mergeSort    (vetor V, int tam);
+
+Desempenho heapSort     (vetor V, int tam);
+
+Desempenho quickSort    (vetor V, int tam);
+
+Desempenho countingSort (vetor V, int tam);
+
+Desempenho bucketSort   (vetor V, int tam);
+
+Desempenho radixSort    (vetor V, int tam);
--- Estrutura_de_Dados/Ordenacao/main.c
+++ Estrutura_de_Dados/Ordenacao/main.c
@@ -1,3 +1,8 @@
+/***********************************************************************************
+    Author:      Lucas Pacheco.
+    Description: Aplicação principal de ordenação.
+    Date:        13/05/2021
+************************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,19 +10,153 @@
 
 vetor V;
 
-int main()
+void clean_screen();
+void mostraDesempenho(Desempenho RD);
+
+int menu()
+{   
+    int op;
+    
+    printf("-----------------------------------------\n");
+    printf(" Algoritmos de Ordenacao com desempenho\n");
+    printf("-----------------------------------------\n");
+
+    printf("Menu:\n");
+    printf("1  - Selection Sort\n");
+    printf("2  - Insertion Sort\n");
+    printf("3  - Bubble Sort\n");
+    printf("4  - Shell Sort\n");
+    printf("5  - Merge Sort\n");
+    printf("6  - Heap Sort\n");
+    printf("7  - Quick Sort\n");
+    printf("8  - Counting Sort\n");
+    printf("9  - Bucket Sort\n");
+    printf("10 - Radix Sort\n");
+    printf("11 - Sair\n");
+    printf("> ");
+    
+    scanf("%d", &op);
+    getchar();
+    
+    clean_screen();
+
+    return (op);
+}
+
+void option(int op)
 {
     int tam;
-
-    printf("Programa de Ordenacao de Vetores\n");
+    Desempenho RD;
 
     printf("Digite o tamanho do vetor: ");
     scanf("%d", &tam); getchar();
 
     geraVetor(V, tam);
-    mostraVetor(V, tam);
-    selectionSort(V, tam);
-    mostraVetor(V, tam);
+    mostraVetor(V, tam, "Original\n");
+
+    switch (op)
+    {
+    case 1:
+        RD = selectionSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Selection sort\n");
+        printf("\n\n\n"); 
+        break;
+    case 2:
+        RD = insertionSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Insertion sort\n");
+        printf("\n\n\n");   
+
+        break;
+
+    case 3:
+        RD = bubbleSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Bubble Sort\n");
+        printf("\n\n\n");
+        break;
+
+    case 4:        
+        RD = shellSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Shell Sort\n");
+        printf("\n\n\n");
+        break;
+
+        break;
+
+    case 5:
+        RD = mergeSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Merge Sort\n");
+        printf("\n\n\n");
+
+        break;
+
+    case 6:
+        RD = heapSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Heap Sort\n");
+        printf("\n\n\n");
+
+        break;
+
+    case 7:
+        RD = quickSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Quick Sort\n");
+        printf("\n\n\n");
+        break;
+
+    case 8:
+        RD = countingSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Counting Sort\n");
+        printf("\n\n\n");
+        break;
+
+    case 9:
+        RD = bucketSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Bucket Sort\n");
+        printf("\n\n\n");
+        break;
+    
+    case 10:
+        RD = radixSort(V, tam);
+        mostraDesempenho(RD);
+        mostraVetor(V, tam, "Radix Sort\n");
+        printf("\n\n\n");
+        break;
+    }
+}
+
+int main()
+{
+    int op;
+
+    do
+        option(op = menu());
+    while (op != 11);
 
     return 0;
+}
+
+void clean_screen()
+{
+#ifdef _WIN32
+    system("cls");
+#endif
+#ifdef linux
+    system("clear");
+#endif
+}
+
+void mostraDesempenho(Desempenho RD)
+{
+    printf("Medidas de desempenho: %ld segundos, ", RD.tempo);
+    printf("%lf comparacoes, ", RD.nCompara);
+    printf("%lf operacoes, ", RD.nOpera);
+    printf("Total = %lf.\n", RD.nOpera + RD.nCompara);
 }
